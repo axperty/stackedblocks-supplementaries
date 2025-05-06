2201cd1e76f346f6fad1a7f94560377154de1dbe
axperty <kevingelhorn0@gmail.com>
2025-05-06 15:39:33 -0300

    Fixed stacked gravel bricks translation and added rope variants.

--- src/main/java/com/axperty/stackedblockssupplementaries/registry/BlockRegistry.java
+++ src/main/java/com/axperty/stackedblockssupplementaries/registry/BlockRegistry.java
@@ -19,16 +19,32 @@ public class BlockRegistry {
     public static final Block STACKED_STONE_TILES = registerBlock("stacked_stone_tiles",
             new Block(AbstractBlock.Settings.copy(Blocks.STONE_BRICKS).mapColor(MapColor.OAK_TAN)));
 
+    // Stacked Stone Tiles (Rope)
+    public static final Block STACKED_STONE_TILES_ROPE = registerBlock("stacked_stone_tiles_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.STONE_BRICKS).mapColor(MapColor.OAK_TAN)));
+
     // Stacked Gravel Bricks
     public static final Block STACKED_GRAVEL_BRICKS = registerBlock("stacked_gravel_bricks",
+            new Block(AbstractBlock.Settings.copy(Blocks.GRAVEL).mapColor(MapColor.OAK_TAN).sounds(BlockSoundGroup.STONE)));
+
+    // Stacked Gravel Bricks (Rope)
+    public static final Block STACKED_GRAVEL_BRICKS_ROPE = registerBlock("stacked_gravel_bricks_rope",
             new Block(AbstractBlock.Settings.copy(Blocks.GRAVEL).mapColor(MapColor.OAK_TAN).sounds(BlockSoundGroup.STONE)));
 
     // Stacked Ash Bricks
     public static final Block STACKED_ASH_BRICKS = registerBlock("stacked_ash_bricks",
             new Block(AbstractBlock.Settings.copy(Blocks.STONE_BRICKS).mapColor(MapColor.OAK_TAN)));
 
+    // Stacked Ash Bricks (Rope)
+    public static final Block STACKED_ASH_BRICKS_ROPE = registerBlock("stacked_ash_bricks_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.STONE_BRICKS).mapColor(MapColor.OAK_TAN)));
+
     // Stacked Lapis Bricks
     public static final Block STACKED_LAPIS_BRICKS = registerBlock("stacked_lapis_bricks",
+            new Block(AbstractBlock.Settings.copy(Blocks.LAPIS_BLOCK).mapColor(MapColor.OAK_TAN).sounds(BlockSoundGroup.DEEPSLATE_TILES)));
+
+    // Stacked Lapis Bricks (Rope)
+    public static final Block STACKED_LAPIS_BRICKS_ROPE = registerBlock("stacked_lapis_bricks_rope",
             new Block(AbstractBlock.Settings.copy(Blocks.LAPIS_BLOCK).mapColor(MapColor.OAK_TAN).sounds(BlockSoundGroup.DEEPSLATE_TILES)));
 
     // Stacked Soap Blocks
@@ -42,22 +58,53 @@ public class BlockRegistry {
                     .sounds(BlockSoundGroup.CORAL)
             ));
 
+    // Stacked Soap Blocks (Rope)
+    public static final Block STACKED_SOAP_BLOCKS_ROPE = registerBlock("stacked_soap_blocks_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)
+                    .slipperiness(0.94f)
+                    //.instrument(NoteBlock.INSTRUMENT)
+                    .mapColor(DyeColor.PINK)
+                    .pistonBehavior(PistonBehavior.PUSH_ONLY)
+                    .strength(1.25F, 4.0F)
+                    .sounds(BlockSoundGroup.CORAL)
+            ));
+
     // Stacked Blackstone Tiles
     public static final Block STACKED_BLACKSTONE_TILES = registerBlock("stacked_blackstone_tiles",
+            new Block(AbstractBlock.Settings.copy(Blocks.BLACKSTONE).mapColor(MapColor.OAK_TAN)));
+
+    // Stacked Blackstone Tiles (Rope)
+    public static final Block STACKED_BLACKSTONE_TILES_ROPE = registerBlock("stacked_blackstone_tiles_rope",
             new Block(AbstractBlock.Settings.copy(Blocks.BLACKSTONE).mapColor(MapColor.OAK_TAN)));
 
     // Stacked Checker Blocks
     public static final Block STACKED_CHECKER_BLOCKS = registerBlock("stacked_checker_blocks",
             new Block(AbstractBlock.Settings.copy(Blocks.STONE).mapColor(MapColor.OAK_TAN)));
 
+    // Stacked Checker Blocks (Rope)
+    public static final Block STACKED_CHECKER_BLOCKS_ROPE = registerBlock("stacked_checker_blocks_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.STONE).mapColor(MapColor.OAK_TAN)));
+
     // Stacked Fine Wood
     public static final Block STACKED_FINE_WOOD = registerBlock("stacked_fine_wood",
+            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS).mapColor(MapColor.OAK_TAN)));
+
+    // Stacked Fine Wood (Rope)
+    public static final Block STACKED_FINE_WOOD_ROPE = registerBlock("stacked_fine_wood_rope",
             new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS).mapColor(MapColor.OAK_TAN)));
 
     // Stacked Daub
     public static final Block STACKED_DAUB = registerBlock("stacked_daub",
             new Block(AbstractBlock.Settings.copy(Blocks.DIRT)
-                    .sounds(ModSoundType.PACKED_MUD)
+                    .sounds(ModSoundType.PACKED_MUD) // Assuming ModSoundType.PACKED_MUD is defined elsewhere
+                    .mapColor(DyeColor.WHITE)
+                    .strength(1.5f, 3.0f)
+            ));
+
+    // Stacked Daub (Rope)
+    public static final Block STACKED_DAUB_ROPE = registerBlock("stacked_daub_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.DIRT)
+                    .sounds(ModSoundType.PACKED_MUD) // Assuming ModSoundType.PACKED_MUD is defined elsewhere
                     .mapColor(DyeColor.WHITE)
                     .strength(1.5f, 3.0f)
             ));
@@ -68,14 +115,32 @@ public class BlockRegistry {
                     .strength(0.5f)
             ));
 
+    // Stacked Feather Blocks (Rope)
+    public static final Block STACKED_FEATHER_BLOCKS_ROPE = registerBlock("stacked_feather_blocks_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)
+                    .strength(0.5f)
+            ));
+
     // Stacked Fodder
     public static final Block STACKED_FODDER = registerBlock("stacked_fodder",
             new Block(AbstractBlock.Settings.copy(Blocks.MOSS_BLOCK)
                     .pistonBehavior(PistonBehavior.NORMAL)
             ));
 
+    // Stacked Fodder (Rope)
+    public static final Block STACKED_FODDER_ROPE = registerBlock("stacked_fodder_rope",
+            new Block(AbstractBlock.Settings.copy(Blocks.MOSS_BLOCK)
+                    .pistonBehavior(PistonBehavior.NORMAL)
+            ));
+
     // Stacked Flint Blocks
     public static final Block STACKED_FLINT_BLOCKS = registerBlock("stacked_flint_blocks",
+            new Block(AbstractBlock.Settings.copy(Blocks.COAL_BLOCK)
+                    .strength(2.0F, 7.5F)
+            ));
+
+    // Stacked Flint Blocks (Rope)
+    public static final Block STACKED_FLINT_BLOCKS_ROPE = registerBlock("stacked_flint_blocks_rope",
             new Block(AbstractBlock.Settings.copy(Blocks.COAL_BLOCK)
                     .strength(2.0F, 7.5F)
             ));
